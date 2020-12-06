270aeeef06d5ea20fb94dd9d201893f6b2a76c5c
Nina Chen <turnipcow@hotmail.com>
2020-12-07 02:31:24 +1100

    next stable build

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # note to self: the smallest entity shouldn't FR back
 class User(AbstractUser):
@@ -7,21 +8,23 @@ class User(AbstractUser):
 
 # required model for auction listings
 class Listing(models.Model):
-    title = models.CharField(max_length=64)
+    title = models.CharField( max_length=64)
     description = models.TextField()
     image_url = models.TextField()
     category = models.CharField(max_length=64)
     is_active = models.BooleanField()
+
+    def valid(self):
+        if "" in [self.title, self.description, self.image_url, self.category]:
+            raise ValidationError('Empty fields.')
+
+    def check_active(self):
+        return self.is_active
+
     def __str__(self):
         return f"{self.title}\n{self.category}\n{self.description}\n{self.image_url}\nis active: {self.is_active}"
 
-# class Comments(models.Model):
-#     num_comments = models.BigIntegerField(default=0)
-#     listing = models.OneToOneField(Listing, on_delete=models.CASCADE, primary_key=False, null=True, blank=True)
-#     def __str__(self):
-#         return f"{self.num_comments} comments in {self.listing}"
 
-'''
 # required model for comments
 class Comment(models.Model):
 
@@ -29,12 +32,17 @@ class Comment(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     # foreign key for many to one relationship where class is the many
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments_listing",  blank=True, null=True)
+
+    
+    def valid(self):
+        if "" == self.comment or not self.listing:
+            raise ValidationError('Empty fields.')
+
     def __str__(self):
         if self.listing and self.comment and self.timestamp:
             return f"{self.listing} [{self.timestamp}]: {self.comment}"
         else:
             return "no comments"
-'''
 
 # required model for bids
 class Bid(models.Model):
@@ -42,6 +50,13 @@ class Bid(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bidding_user",  blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bid_listing",  blank=True, null=True)
+
+
+    def valid(self):
+        if not price or not user or not listing:
+            raise ValidationError('Empty fields.')
+
+
     def __str__(self):
         return f"[{self.timestamp}]: {self.user} bidding at ${self.price} for {self.listing}"
 
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -4,14 +4,17 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User, Bid, Listing#, Comments, Watchlist, Comment
+from .models import User, Bid, Listing, Comment#, Watchlist
 
 def create_listing(request):
     return render(request, "auctions/create_listing.html")
 
 def index(request):
+    print(list(Bid.objects.all()))
+    print(list(Bid.objects.all()))
     return render(request, "auctions/index.html", {
-        "listings": Listing.objects.all()
+        "listings": Listing.objects.all(),
+        "all_bids": Bid.objects.all(),
     })
 
 def login_view(request):
@@ -39,26 +42,32 @@ def logout_view(request):
 
 def create(request):
     if request.method == "POST":
-        # try:
-        listing = Listing(
-            title = request.POST["title"],
-            description = request.POST['description'],
-            image_url = request.POST["image_url"],
-            category = request.POST["category"],
-            is_active = 1,
-        )
-        print(listing)
-        listing.save()
-        print("saved")
-        Bid.objects.create(
-            price = float(request.POST["starting_bid"]), 
-            user = request.user,
-            listing = listing,
-        )
-        print(listing)
-        return HttpResponseRedirect(reverse("index"))
-        # except:
-        #     return render(request, "auctions/error.html")
+        try:
+            listing = Listing(
+                title = request.POST["title"],
+                description = request.POST['description'],
+                image_url = request.POST["image_url"],
+                category = request.POST["category"],
+                is_active = 1,
+            )
+            listing.valid()
+            listing.save()
+            print(listing)
+            bid = Bid(
+                price = float(request.POST["starting_bid"]), 
+                user = request.user,
+                listing = listing,
+            )
+            bid.save()
+            print(bid)
+
+
+
+            return HttpResponseRedirect(reverse("index"))
+        except:
+            return render(request, "auctions/create_listing.html", {
+                'message': "Please fill out all fields in the correct format",
+            })
 
 def register(request):
     if request.method == "POST":
